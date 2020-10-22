4a95fb2222c88925777d41e7da42f25f6f6fcd2e
Riley Harper <63863844+rcharper100@users.noreply.github.com>
2020-10-22 10:02:34 -0400

    Initial Commit

---
+++ vZHU IDSTests/vZHU_IDSTests.swift
@@ -0,0 +1,33 @@
+//
+//  vZHU_IDSTests.swift
+//  vZHU IDSTests
+//
+//  Created by Riley Harper on 10/22/20.
+//
+
+import XCTest
+@testable import vZHU_IDS
+
+class vZHU_IDSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
