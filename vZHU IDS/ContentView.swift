b7c3e6137ddfca0a4155551aa71b8c6e0c839cbb
Riley Harper <63863844+rcharper100@users.noreply.github.com>
2020-10-27 10:28:08 -0400

    Squashed some bugs
    ContentView not loading in correctly, removed WKWebView to add separate API view and PDFView

--- vZHU IDS/ContentView.swift
+++ vZHU IDS/ContentView.swift
@@ -4,19 +4,12 @@
 //
 //  Created by Riley Harper on 10/22/20.
 //Hi Romashov
+//
 
 import SwiftUI
-import WebKit
-import UIKit
-class ViewController: UIViewController, WKUIDelegate {
-var webView: WKWebView!
 
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
-    }
+
+    
 struct ContentView: View {
 
     @State private var selection = 1
@@ -40,12 +33,12 @@ struct ContentView: View {
             Text("Third Content View")
                 .tabItem {
                     Image(systemName: "cloud.bolt.rain")
-                    Text("Current vATIS")
+                    Text("Weather (vATIS)")
                 }.tag(3)
             Text("Fourth Content View")
                 .tabItem {
                     Image(systemName: "airplane")
-                    Text("Event Info")
+                    Text("Traffic Management")
                 }.tag(4)
         }
         
@@ -79,4 +72,4 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-}
+
