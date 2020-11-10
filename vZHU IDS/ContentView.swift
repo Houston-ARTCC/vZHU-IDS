11aaf49e56f8e111e91a3d84e76bc732a0908af8
Riley Harper <63863844+rcharper100@users.noreply.github.com>
2020-11-10 11:06:22 -0500

    Added Support for vATIS API (Thanks Romashov)
    Here's some SHIT code for adding the API, will polish more for the lovely final product.

--- vZHU IDS/ContentView.swift
+++ vZHU IDS/ContentView.swift
@@ -3,9 +3,9 @@
 //  vZHU IDS
 //
 //  Created by Riley Harper on 10/22/20.
-//Hi Romashov
-//
-//
+//  Hi Romashov
+//  Welcome Britain
+//  Keep America Hot
 
 
 import SwiftUI
@@ -19,32 +19,40 @@ struct ContentView: View {
     var body: some View {
 
         TabView() {
+            
             Text("Pre-Duty Items")
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Pre-Duty")
                 }.tag(1)
 
-            Text("Second Content View")
-                .tabItem {
-                    Image(systemName: "paperclip")
-                    Text("Documents")
-                }.tag(2)
+            var url : String = "https://zhuartcc.org/api/vatis/"
+            var request : NSMutableURLRequest = NSMutableURLRequest()
+            request.URL = NSURL(string: url)
+            request.HTTPMethod = "GET"
 
-            Text("Third Content View")
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+                let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+
+                if (jsonResult != nil) {
+                    // process jsonResult
+                } else {
+                   Text("No vATIS Facility Found Ensure Using Latest vATIS Profile")
+                }
                 .tabItem {
                     Image(systemName: "cloud.bolt.rain")
                     Text("Weather (vATIS)")
-                }.tag(3)
+                }.tag(2)
             Text("Fourth Content View")
                 .tabItem {
                     Image(systemName: "airplane")
                     Text("TMU Notices")
-                }.tag(4)
+                }.tag(3)
         }
         
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -74,3 +82,4 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
+}
