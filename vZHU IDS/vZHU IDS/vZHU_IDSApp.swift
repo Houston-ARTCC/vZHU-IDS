e76c01acfdc655af4b1ef17065b496a7e10aa6bd
Riley Harper <63863844+rcharper100@users.noreply.github.com>
2020-11-30 22:23:31 -0500

    Base Project files
    Added Base Project files Xcode generates after cleaning house

---
+++ vZHU IDS/vZHU IDS/vZHU_IDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  vZHU_IDSApp.swift
+//  vZHU IDS
+//
+//  Created by Riley Harper on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct vZHU_IDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
